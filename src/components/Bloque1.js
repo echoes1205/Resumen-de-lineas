40cbbd9382fbf6c3929331387506601daec4c470
echoes1205 <leonardo.ochoaravelo@gmail.com>
2023-11-17 13:11:08 -0700

    edit-button

--- src/components/Bloque1.js
+++ src/components/Bloque1.js
@@ -1,8 +1,9 @@
 //useState sirve para que los datos de una variable se actualicen inmediatamente
 import './Bloque1.css'
 import React, { useEffect, useState } from 'react'
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from '@mui/icons-material/Edit'
 import Table from 'react-bootstrap/Table'
+import { IconButton } from '@mui/material'
 
 function App () {
   const [data, setData] = useState([])
@@ -18,9 +19,9 @@ function App () {
   return (
     <div className='tabla-lineas'>
       <h1 className='title'>Reporte de Bloque 1</h1>
-      <EditIcon className='editbutton'>
+      {/* <EditIcon className='editbutton'></EditIcon> */}
 
-      </EditIcon>
+      
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -28,7 +29,8 @@ function App () {
             <th>Ordenes</th>
             <th>Horas</th>
             <th>Defectos</th>
-            <th>OPF</th>
+            <th>Cumpliendo OPF</th>
+            <th className='editfield'>Editar</th>
           </tr>
         </thead>
         <tbody>
@@ -38,13 +40,19 @@ function App () {
               <td>{d.ordenes}</td>
               <td>{d.horas}</td>
               <td>{d.defectos}</td>
-              <td>{d.opf}</td>
+              <td>{d.opf} </td>
+              <td>
+                <IconButton className='editbutton' aria-label='edit'>
+                  <EditIcon />
+                </IconButton>
+              </td>
             </tr>
           ))}
+          
         </tbody>
       </Table>
     </div>
   )
 }
 
-export default App;
+export default App
