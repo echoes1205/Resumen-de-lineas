f71a0cfb7b9e0ae2dc8dae08ae88e5f0e293b492
echoes1205 <leonardo.ochoaravelo@gmail.com>
2023-11-22 11:37:59 -0700

    put-method working

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
-
-
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors')
-
-
 const app = express()
+
 app.use(cors())
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -26,6 +24,50 @@ app.get('/bloque1', (req, res)=> {
         return res.json(data);
     })
 })
+
+//update
+app.put("/bloque1/:id", (req, res) => {
+    const { id } = req.params;
+    const { ordenes, horas, defectos, opf } = req.body;
+    const sql = `UPDATE bloque1 SET ordenes = ?, horas = ?, defectos = ?, opf = ? WHERE id = ?`;
+    db.query(sql, [ordenes, horas, defectos, opf, id], (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
+
+// app.post("/bloque1/:id",(req,res)=>{
+//     // const {id}=req.params;
+//     const {ordenes, horas, defectos, opf}=req.body;
+//     const sql='insert into bloque1 values (?,?,?,?)';
+//     db.query(sql, (err,data)=>{
+//         if(err)
+//         res.json({err: err});
+//         else
+//         res.json(data);
+//     })
+// })
+
+// insert
+app.post('/bloque1:id', (req, res) => {
+    const { ordenes, horas, defectos, opf } = req.body;
+  
+    const sql = `INSERT INTO bloque1 (ordenes, horas, defectos, opf) VALUES (?, ?, ?, ?)`;
+  
+    db.query(sql, [ordenes, horas, defectos, opf], (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
+
 
 app.listen(8081, ()=> {
     console.log("Servidor corriendo en http://localhost:8081/");
