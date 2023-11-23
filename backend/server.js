2c234ead7d0be0c4769404c94ff5bf0ea1d76737
echoes1205 <leonardo.ochoaravelo@gmail.com>
2023-11-23 13:43:42 -0700

    axios-feature

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,77 @@
 const express = require('express');
 const mysql = require('mysql');
-const cors = require('cors')
+const cors = require('cors');
 const app = express()
 
 app.use(cors())
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"",
-    database:"resumen-lineas"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "resumen-lineas"
 })
 
-app.get('/', (re, res)=> {
-    return res.json("From backend side");
+app.get('/', (re, res) => {
+  return res.json("From backend side");
 })
 
-app.get('/bloque1', (req, res)=> {
-    const sql = "SELECT * FROM bloque1";
-    db.query(sql, (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
+
+
+app.get('/bloque1', (req, res) => {
+  const sql = "SELECT * FROM bloque1";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  })
 })
+
+app.get('/bloque1/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const sql = `SELECT * FROM bloque1`;
+  const values = [id];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error fetching record:', err);
+      return res.json(data);
+  }})
+})
+
+
+//get de pesta;a update
+app.get('/bloque1/update/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const sql = `SELECT * FROM bloque1`;
+  const values = [id];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error fetching record:', err);
+      return res.json(data);
+  }})
+})
+
+
+
+
+
 
 //update
 app.put("/bloque1/:id", (req, res) => {
-    const { id } = req.params;
-    const { ordenes, horas, defectos, opf } = req.body;
-    const sql = `UPDATE bloque1 SET ordenes = ?, horas = ?, defectos = ?, opf = ? WHERE id = ?`;
-    db.query(sql, [ordenes, horas, defectos, opf, id], (err, result) => {
-      if (err) {
-        res.send({ err: err });
-      } else {
-        res.send(result);
-      }
-    });
+  const { id } = req.params;
+  const { ordenes, horas, defectos, opf } = req.body;
+  const sql = `UPDATE bloque1 SET ordenes = ?, horas = ?, defectos = ?, opf = ? WHERE id = ?`;
+  db.query(sql, [ordenes, horas, defectos, opf, id], (err, result) => {
+    if (err) {
+      res.send({ err: err });
+    } else {
+      res.send(result);
+    }
   });
-  
+});
+
 
 // app.post("/bloque1/:id",(req,res)=>{
 //     // const {id}=req.params;
@@ -54,21 +87,21 @@ app.put("/bloque1/:id", (req, res) => {
 
 // insert
 app.post('/bloque1:id', (req, res) => {
-    const { ordenes, horas, defectos, opf } = req.body;
-  
-    const sql = `INSERT INTO bloque1 (ordenes, horas, defectos, opf) VALUES (?, ?, ?, ?)`;
-  
-    db.query(sql, [ordenes, horas, defectos, opf], (err, result) => {
-      if (err) {
-        res.send({ err: err });
-      } else {
-        res.send(result);
-      }
-    });
+  const { ordenes, horas, defectos, opf } = req.body;
+
+  const sql = `INSERT INTO bloque1 (ordenes, horas, defectos, opf) VALUES (?, ?, ?, ?)`;
+
+  db.query(sql, [ordenes, horas, defectos, opf], (err, result) => {
+    if (err) {
+      res.send({ err: err });
+    } else {
+      res.send(result);
+    }
   });
-  
+});
 
 
-app.listen(8081, ()=> {
-    console.log("Servidor corriendo en http://localhost:8081/");
+
+app.listen(8081, () => {
+  console.log("Servidor corriendo en http://localhost:8081/");
 })
